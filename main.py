923cb69941a57007485f8d49f5995ca752d9843f
samskritbharatnicin-dot <samskritbharat.nic.in@gmail.com>
2026-09-11 10:36:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def chat_endpoint(req: ChatRequest):
     
     try:
         response = client.models.generate_content(
-            model="model="gemini-2.5-flash",
+            model="model="model=gemini-3.6-flash"
             contents=req.message,
             config=types.GenerateContentConfig(
                 system_instruction=SYSTEM_INSTRUCTION,
